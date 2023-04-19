cccecb49ef323bcc0378cfc19698bb99bf7773fc
imrandev20 <imrandev20@gmail.com>
2023-04-20 05:49:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorType, createChart } from "lightweight-charts";
+import React, { useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const chartContainerRef = useRef();
+
+  useEffect(() => {
+    const initialData = [
+      { time: "2018-12-22", value: 32.51 },
+      { time: "2018-12-23", value: 31.11 },
+      { time: "2018-12-24", value: 27.02 },
+      { time: "2018-12-25", value: 27.32 },
+      { time: "2018-12-26", value: 25.17 },
+      { time: "2018-12-27", value: 28.89 },
+      { time: "2018-12-28", value: 25.46 },
+      { time: "2018-12-29", value: 23.92 },
+      { time: "2018-12-30", value: 22.68 },
+      { time: "2018-12-31", value: 22.67 },
+    ];
+
+    const chart = createChart(chartContainerRef.current, {
+      layout: {
+        background: { type: ColorType.Solid, color: "white" },
+      },
+      width: chartContainerRef.current.clientWidth,
+      height: 500,
+    });
+
+    const newSeries = chart.addAreaSeries({
+      lineColor: "#2962FF",
+      topColor: "#2962FF",
+      bottomColor: "rgba(41, 98, 255, 0.28)",
+    });
+
+    newSeries.setData(initialData);
+
+    return () => [chart.remove()];
+  }, []);
+
+  return <div ref={chartContainerRef}></div>;
 }
 
 export default App;
+
+// { lineColor: '#2962FF', topColor: '#2962FF', bottomColor: 'rgba(41, 98, 255, 0.28)' } - areaSeries
