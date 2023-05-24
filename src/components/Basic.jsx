52b600f69fc229e3c3c7335e31e47ee5a3fad274
imrandev20 <imrandev20@gmail.com>
2023-05-24 10:05:53 +0600

    probably made some changes

--- src/components/Basic.jsx
+++ src/components/Basic.jsx
@@ -6543,11 +6543,27 @@ const Basic = () => {
       </ResizableBox>
       <div
         style={{
-          backgroundColor: "green",
+          backgroundColor: "#222",
+          color: "white",
           flexGrow: 1,
         }}
       >
-        Hello
+        <ul
+          style={{
+            listStyle: "none",
+          }}
+        >
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Option 3</li>
+          <li>Option 4</li>
+          <li>Option 5</li>
+
+          <li>Option 6</li>
+          <li>Option 7</li>
+          <li>Option 8</li>
+          <li>Option 9</li>
+        </ul>
       </div>
     </div>
   );
